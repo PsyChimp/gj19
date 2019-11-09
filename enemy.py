55ec05e781d66f2067634514f2500c34d97d72e9
Brandon <32993684+PsyChimp@users.noreply.github.com>
2019-11-09 14:32:44 -0500

    progress on pathfinding

--- enemy.py
+++ enemy.py
@@ -4,6 +4,8 @@ from pygame.locals import *
 
 from globals import *
 
+import queue
+
 class Enemy(object):
     class AIState(Enum):
         Idle = 0
@@ -12,13 +14,18 @@ class Enemy(object):
         self.game = game
         self.pos = pygame.math.Vector2(WIN_WIDTH_PX / 2, WIN_HEIGHT_PX / 2)
         self.vel = pygame.math.Vector2(0, 0)
-        self.img = self.game.player_img
+        #self.img = self.game.player_img
         self.radius = 16
         self.can_move_x = True
         self.can_move_y = True
         self.state = self.AIState.Idle
     def update(self):
         # Update velocity
+        path = self.find_path_to_player()
+        #if(self.get_player_tile_pos() in path):
+            #next = path[self.get_player_tile_pos()]
+            #print(next)
+        print(path)
         self.vel.x = (self.game.keys[K_RIGHT] - self.game.keys[K_LEFT])
         self.vel.y = (self.game.keys[K_DOWN] - self.game.keys[K_UP])
         if self.vel.x != 0 or self.vel.y != 0:
@@ -43,7 +50,7 @@ class Enemy(object):
                 self.can_move_x = False
             if self.can_move_y and new_yrect.colliderect(w):
                 self.can_move_y = False
-
+        
         # Update position
         if self.can_move_x:
             self.pos.x = new_pos.x
@@ -51,10 +58,52 @@ class Enemy(object):
             self.pos.y = new_pos.y
         self.can_move_x, self.can_move_y = True, True
         
-        print(self.get_player_tile_pos())
-        
     def draw(self):
         x, y = self.pos.x - 16, self.pos.y - 16
         pygame.draw.rect(self.game.screen, RED, (x, y, 32, 32))
+        
     def get_player_tile_pos(self):
         return self.game.get_tile_pos(self.game.player.pos)
+    
+    def get_self_tile_pos(self):
+        return self.game.get_tile_pos(self.pos)
+    
+    def get_map_open_tile_pos(self):
+        open_spaces = []
+        map = ROOMS[self.game.cur_room]
+        y = 0
+        for row in map:
+            x = 0
+            for tile in row:
+                if(tile == '.'):
+                    open_spaces.append((x,y))
+                x+=1
+            y+=1
+        return open_spaces
+        
+    def get_open_neighbors(self,pos):
+        open = self.get_map_open_tile_pos()
+        neighbors = []
+        for i in range(-1, 1):
+            for j in range(-1,1):
+                n = (pos[0] - i, pos[1] - j)
+                if(n in open):
+                    neighbors.append(n)
+        return neighbors
+        
+    def find_path_to_player(self):
+        start = self.get_self_tile_pos()
+        end = self.get_player_tile_pos()
+        frontier = queue.Queue()
+        frontier.put(start)
+        came_from = {}
+        came_from[start] = None
+        while not frontier.empty():
+            current = frontier.get()
+            if(current == end):
+                break
+            for next in self.get_open_neighbors(current):
+                if next not in came_from:
+                    frontier.put(next)
+                    came_from[next] = current
+        return came_from
