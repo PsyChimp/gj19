ef3ce53a0f376d4da9c1b4b8c702f2e699321f34
m-gaston <35315240+m-gaston@users.noreply.github.com>
2019-11-09 11:15:46 -0500

    Added button class

---
+++ button.py
@@ -0,0 +1,73 @@
+import pygame
+from pygame.locals import *
+
+class Button(object):
+    def __init__(self, rect, colors, font, text, on_click, *args):
+        self.rect = rect
+        self.colors = colors
+        self.text_surf = font.render(text, True, colors["text"])
+        dims = self.text_surf.get_size()
+        self.text_pos = (
+            self.rect.centerx - dims[0] / 2,
+            self.rect.centery - dims[1] / 2)
+        self.on_click = on_click
+        self.args = args
+        self.state = "default"
+
+    def handle_event(self, event):
+        if self.state == "disabled":
+            return
+        self.state = "default"
+        mpos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mpos):
+            self.state = "hovered"
+        if self.state == "hovered":
+            if event.type == MOUSEBUTTONDOWN and event.button == 1:
+                self.state = "clicked"
+                self.on_click(*self.args)
+        if event.type == MOUSEBUTTONUP:
+            self.state = "default"
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.colors[self.state], self.rect)
+        surface.blit(self.text_surf, self.text_pos)
+
+if __name__ == "__main__":
+    def foo():
+        print("bar")
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((220, 130))
+    font = pygame.font.Font(None, 16)
+    h = font.get_height()
+
+    colors = {
+        "default": (51, 153, 255),
+        "hovered": (0, 128, 255),
+        "clicked": (0, 102, 204),
+        "disabled": (128, 128, 128),
+        "text": (255, 255, 255)}
+
+    buttons = [
+        Button(pygame.Rect(10, 10, 200, 50), colors, font, "Button 1", foo),
+        Button(pygame.Rect(10, 70, 200, 50), colors, font, "Button 2", foo)]
+
+    while True:
+        e = pygame.event.poll()
+        if e.type == QUIT:
+            break
+        elif e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                break
+
+        for b in buttons:
+            b.handle_event(e)
+
+        screen.fill((255, 255, 255))
+        for b in buttons:
+            b.draw(screen)
+
+        pygame.display.flip()
+
+    pygame.quit()
