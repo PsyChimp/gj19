03fec950c1e935015f0fe388e087733c526f784b
m-gaston <35315240+m-gaston@users.noreply.github.com>
2019-11-10 11:11:35 -0500

    Revert "Renamed OneHpDowngrade() to OneHPDowngrade()"
    This reverts commit 6af8f249e8c0d25490d40d696b93e132c5bb10a6.

--- downgrade.py
+++ downgrade.py
@@ -10,6 +10,6 @@ class Downgrade(object):
     def apply(self):
         return NotImplementedError
 
-class OneHPDowngrade(Downgrade):
+class OneHpDowngrade(Downgrade):
     def apply(self):
         self.game.player.hp = 1
